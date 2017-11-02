1f2ef0459a13081c8fdcf3cebece9930ddd3d341
Nichanan Kesonpat <n.kesonpat@gmail.com>
2017-11-02 20:08:03 +0000

    Added image size validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,5 +3,14 @@ class Image < ApplicationRecord
   
   #Uses carrierwave to associate image with the model. :picture is an attr name of type str
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+  
+  def picture_size
+      if picture.size > 5.megabytes
+          errors.add(:picture, "should be less than 5MB")
+      end
+  end
 
 end
